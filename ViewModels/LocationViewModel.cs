0c86fdf7ab901c356520cf8b687f53e32dc72db2
Clibzz <chris.klunder@student.nhlstenden.com>
2024-12-02 00:48:58 +0100

    Added a (temporary) way to remove saved places

--- ViewModels/LocationViewModel.cs
+++ ViewModels/LocationViewModel.cs
@@ -19,9 +19,12 @@ namespace WeatherApp.ViewModels
 
         public ICommand SearchCommand { get; }
 
+        public ICommand RemoveLocationCommand { get; }
+
         public LocationViewModel()
         {
             _api = new GeocodingAPI();
+            RemoveLocationCommand = new Command<LocationModel>(RemoveLocation);
             SavedLocations = new ObservableCollection<LocationModel>();
             SearchResults = new ObservableCollection<LocationModel>();
             SearchCommand = new Command(async () => await PerformSearch());
@@ -40,6 +43,41 @@ namespace WeatherApp.ViewModels
                     SearchCommand.Execute(null);
                 }
             }
+        }
+
+        private void RemoveLocation(LocationModel location)
+        {
+            if (SavedLocations.Contains(location))
+            {
+                SavedLocations.Remove(location);
+                UpdatePlacesJson();
+            }
+        }
+
+        private void UpdatePlacesJson()
+        {
+            try
+            {
+                if (File.Exists(GetFilePath()))
+                {
+                    string json = JsonConvert.SerializeObject(SavedLocations.ToList(), Formatting.Indented);
+                    File.WriteAllText(GetFilePath(), json);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to update places.json: {ex.Message}");
+            }
+        }
+
+        private string GetFilePath()
+        {
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string solutionDirectory = Path.GetFullPath(Path.Combine(currentDirectory, @"..\..\..\..\..\")); // Go up to the solution directory
+            string testDataPath = Path.Combine(solutionDirectory, "TestData");
+            string filePath = Path.Combine(testDataPath, "places.json");
+
+            return filePath;
         }
 
         private async Task PerformSearch()
@@ -63,12 +101,7 @@ namespace WeatherApp.ViewModels
 
         public void LoadSavedLocations()
         {
-            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string solutionDirectory = Path.GetFullPath(Path.Combine(currentDirectory, @"..\..\..\..\..\")); // Go up to the solution directory
-            string testDataPath = Path.Combine(solutionDirectory, "TestData");
-            string filePath = Path.Combine(testDataPath, "places.json");
-
-            var savedLocations = LoadLocationsFromFile(filePath);
+            var savedLocations = LoadLocationsFromFile(GetFilePath());
 
             // Clear existing data and add new saved locations
             SavedLocations.Clear();
