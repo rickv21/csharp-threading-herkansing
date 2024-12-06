95735880d3f50a5edd6bd2b95ee7854703a02cac
Monique S <moniquesbg@gmail.com>
2024-12-06 16:17:47 +0100

    setup weatherAPI class

---
+++ WeatherAPIs/WeatherAPI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.WeatherAPIs
+{
+    internal class WeatherAPI : WeatherService
+    {
+        public WeatherAPI() : base("WeatherAPI", "http://api.weatherapi.com/v1", 300, -1)
+        {
+        }
+
+        public override Task<APIResponse<List<WeatherDataModel>>> GetWeatherDataAsync(DateTime day, LocationModel location, bool simulate = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<APIResponse<List<WeatherDataModel>>> GetWeatherForAWeekAsync(LocationModel location, bool simulate = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override WeatherCondition CalculateWeatherCondition(object data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
